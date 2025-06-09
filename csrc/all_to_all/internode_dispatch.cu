61b9602ade94a1d621fa29fb4c1995510131c7bd
Artem Y. Polyakov <artemp@nvidia.com>
2025-06-09 11:36:22 -0400

    fix prev

--- csrc/all_to_all/internode_dispatch.cu
+++ csrc/all_to_all/internode_dispatch.cu
@@ -233,7 +233,7 @@ __global__ __launch_bounds__(NUM_WARPS * 32, 1) void dispatchKernel(
         uint32_t token = tokenStart + i;
 #if FORCE_ZCOPY
         // Unused in this case
-        sourceIndex[token] = (unsigned)(-1);
+        sourceIndex[token] = i;
 #else
         sourceIndex[token] = *((uint32_t *)(xTokenBuffer + tokenDim));
 #endif
